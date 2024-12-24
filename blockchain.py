be8a4bee4241958b84bc36fbd0a99e99e740224c
Thomas Ketterer <thomasketterer3@gmail.com>
2024-12-24 13:31:16 -0500

    50% this 50% studying for Sec+

--- blockchain.py
+++ blockchain.py
@@ -62,3 +62,34 @@ class Blockchain:
         
 app = Flask(__name__)
 
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.print_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {'message': 'A block is MINED', 'index': block['index'], 'timestamp': block['timestamp'], 'proof': block['proof'], 'previous_hash': block['previous_hash']}
+
+    return jsonify(response), 200
+@app.route('/get_chain', methods=['GET'])
+def display_chain():
+    response = {'chain': blockchain.chain, 'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+@app.route('/valid', methods=['GET'])
+def valid():
+    valid = blockchain.chain_valid(blockchain.chain)
+
+    if valid:
+        response = {'message': 'The Blockchain is valid.'}
+    else:
+        response = {'message': 'The Blockchain is not valid'}
+    return jsonify(response), 200
+
+app.run(host='0.0.0.0', port=5000)
+
+
